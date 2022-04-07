0f9fd79b4e279effbd4fcf766f64298f3ad82f7e
eve000017 <eve000017@gmail.com>
2022-04-07 09:46:30 +0800

    edit the octokit in index.js

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -3,13 +3,16 @@ const github = require("@actions/github");
 
 async function run() {
   try {
+    //get inputs from action.yml
     const issueTitle = core.getInput("issue-title");
     const jokeBody = core.getInput("joke");
     const token = core.getInput("repo-token");
 
     const octokit = new github.getOctokit(token);
 
-    const newIssue = await octokit.issues.create({
+    //updated from web https://github.com/githubtraining/write-javascript-actions/issues/36?msclkid=b65d4539b61311ecae9087e4f6a7a553
+    // need to use "await octokit.rest.issue.create" instead of await octokit.await.issue.create"
+    const newIssue = await octokit.rest.issues.create({
       repo: github.context.repo.repo,
       owner: github.context.repo.owner,
       title: issueTitle,
